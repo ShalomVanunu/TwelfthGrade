35ec4ddf2353dfa2fbd38c594482340dea723194
ShalomV <shalomvanunu@gmail.com>
2020-09-18 14:48:53 +0300

    Updated 18/9/2020

--- SQLite/SQLiteInsertData.py
+++ SQLite/SQLiteInsertData.py
@@ -1,7 +1,7 @@
 import sqlite3
 #from sqlite3 import *
 
-conn = sqlite3.connect('Class12.sqlite') #connection to DB
+conn = sqlite3.connect('TwelfthGrade.sqlite') #connection to DB
 
 connect_to_db = conn.cursor() # set the point to the start of DB
 
